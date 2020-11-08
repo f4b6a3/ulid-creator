73d43dfc417cc6ccfc0f7578a1db83d6bcff5c4e
Fabio Lima <fabiolimace@gmail.com>
2020-11-08 11:15:22 -0300

    [#5] Generate ULID compatible with RFC-4122 UUID v4
    Now it can generate UUIDs and ULIDs compatible with RFC-4122 UUID v4.
    
    List of Changes:
    
    Created UlidCreator.getUlid4();
    Created UlidCreator.getUlidString4();
    Updated README.md
    
    Coverage 94.1%

--- README.md
+++ README.md
@@ -33,7 +33,7 @@ Add these lines to your `pom.xml`.
 <dependency>
   <groupId>com.github.f4b6a3</groupId>
   <artifactId>ulid-creator</artifactId>
-  <version>2.2.0</version>
+  <version>2.3.0</version>
 </dependency>
 ```
 See more options in [maven.org](https://search.maven.org/artifact/com.github.f4b6a3/ulid-creator).
@@ -52,6 +52,12 @@ The default random number generator is `java.security.SecureRandom`.
 ```java
 // GUID based on ULID spec
 UUID ulid = UlidCreator.getUlid();
+```
+
+```java
+// GUID based on ULID spec
+// Compatible with RFC-4122 UUID v4
+UUID ulid = UlidCreator.getUlid4();
 ```
 
 Sequence of GUIDs based on ULID spec:
@@ -88,6 +94,12 @@ See the section on GUIDs to know how the 128 bits are generated in this library.
 ```java
 // String based on ULID spec
 String ulid = UlidCreator.getUlidString();
+```
+
+```java
+// String based on ULID spec
+// Compatible with RFC-4122 UUID v4
+String ulid = UlidCreator.getUlidString4();
 ```
 
 Sequence of Strings based on ULID spec:
--- src/test/java/com/github/f4b6a3/ulid/ulid/UlidCreatorTest.java
+++ src/test/java/com/github/f4b6a3/ulid/ulid/UlidCreatorTest.java
@@ -3,12 +3,14 @@ package com.github.f4b6a3.ulid.ulid;
 import org.junit.Test;
 
 import com.github.f4b6a3.ulid.UlidCreator;
+import com.github.f4b6a3.ulid.util.UlidConverter;
 import com.github.f4b6a3.ulid.util.UlidUtil;
 import com.github.f4b6a3.ulid.util.UlidValidator;
 
 import static org.junit.Assert.*;
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.UUID;
 
 public class UlidCreatorTest {
 
@@ -31,6 +33,25 @@ public class UlidCreatorTest {
 		checkUniqueness(list);
 		checkOrdering(list);
 		checkCreationTime(list, startTime, endTime);
+	}
+	
+	@Test
+	public void testGetUlid4() {
+		String[] list = new String[DEFAULT_LOOP_MAX];
+
+		long startTime = System.currentTimeMillis();
+
+		for (int i = 0; i < DEFAULT_LOOP_MAX; i++) {
+			list[i] = UlidCreator.getUlidString4();
+		}
+
+		long endTime = System.currentTimeMillis();
+
+		checkNullOrInvalid(list);
+		checkUniqueness(list);
+		checkOrdering(list);
+		checkCreationTime(list, startTime, endTime);
+		checkVersion4(list);
 	}
 
 	private void checkNullOrInvalid(String[] list) {
@@ -71,6 +92,13 @@ public class UlidCreatorTest {
 
 		for (int i = 0; i < list.length; i++) {
 			assertEquals("The ULID list is not ordered", list[i], other[i]);
+		}
+	}
+	
+	private void checkVersion4(String[] list) {
+		for (String ulid : list) {
+			UUID uuid = UlidConverter.fromString(ulid);
+			assertEquals(String.format("ULID is is not version 4 %s", uuid), 4, uuid.version());
 		}
 	}
 }
